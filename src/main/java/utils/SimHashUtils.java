bcfb600f79cb8e6f4bb0323c65685b72c3b23fda
rujiefenfang <1473551776@qq.com>
2021-09-18 19:36:26 +0800

    第二次提交：获得simHash值

--- src/main/java/utils/SimHashUtils.java
+++ src/main/java/utils/SimHashUtils.java
@@ -1,27 +1,79 @@
 package utils;
 
 
+import com.hankcs.hanlp.HanLP;
+
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 public class SimHashUtils {
 
 
     /**
-     *MessageDigest 类为应用程序提供信息摘要算法的功能，利用SHA算法。接收任意大小的字符串，返回固定长度的哈希值。
+     * MessageDigest 类为应用程序提供信息摘要算法的功能，利用SHA算法。接收任意大小的字符串，返回固定长度的哈希值。
+     *
      * @param str
      * @return str的哈希值
      */
     public static String getHash(String str){
-        try {
+        try{
+            // 这里使用了SHA获得hash值
             MessageDigest messageDigest = MessageDigest.getInstance("SHA");
-            return new BigInteger(1,messageDigest.digest(str.getBytes(StandardCharsets.UTF_8))).toString();
-        } catch (NoSuchAlgorithmException e) {
+            return new BigInteger(1, messageDigest.digest(str.getBytes("UTF-8"))).toString(2);
+        }catch(Exception e){
             e.printStackTrace();
             return str;
         }
     }
 
+    /**
+     * 传入String,计算出它的simHash值，并以字符串形式输出
+     * @param str 传入的Srting类型字符串
+     * @return 返回str的simHash值
+     */
+    public static String getSimHash(String str){
+        // 用数组表示特征向量,取128位,从 0 1 2 位开始表示从高位到低位
+        int[] v = new int[128];
+        // 1、分词（使用了外部依赖hankcs包提供的接口）
+        List<String> keywordList = HanLP.extractKeyword(str, str.length());//取出所有关键词
+        // hash
+        int size = keywordList.size();
+        int i = 0;//以i做外层循环
+        for(String keyword : keywordList){
+            // 2、获取hash值
+            String keywordHash = getHash(keyword);
+            if (keywordHash.length() < 128) {
+                // hash值可能少于128位，在低位以0补齐
+                int dif = 128 - keywordHash.length();
+                for (int j = 0; j < dif; j++) {
+                    keywordHash += "0";
+                }
+            }
+            // 3、加权、合并
+            for (int j = 0; j < v.length; j++) {
+                // 对keywordHash的每一位与'1'进行比较
+                if (keywordHash.charAt(j) == '1') {
+                    //权重分10级，由词频从高到低，取权重10~0
+                    v[j] += (10 - (i / (size / 10)));
+                } else {
+                    v[j] -= (10 - (i / (size / 10)));
+                }
+            }
+            i++;
+        }
+        // 4、降维
+        String simHash = "";// 储存返回的simHash值
+        for (int j = 0; j < v.length; j++) {
+            // 从高位遍历到低位
+            if (v[j] <= 0) {
+                simHash += "0";
+            } else {
+                simHash += "1";
+            }
+        }
+        return simHash;
+    }
 }
--- src/test/java/SimHashUtilsTest.java
+++ src/test/java/SimHashUtilsTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import utils.SimHashUtils;
+import utils.TxtUtils;
 
 public class SimHashUtilsTest {
 
@@ -7,5 +8,16 @@ public class SimHashUtilsTest {
     public void getHashTest(){
         System.out.println(SimHashUtils.getHash("我今天早上要去打码"));
         System.out.println(SimHashUtils.getHash("我今天早上不要去打码"));
+    }
+    @Test
+    public void getSimHashTest(){
+        String filePath1 = "D:\\JAVA\\demo\\PaperChech\\src\\main\\resources\\txt文件\\测试文本\\orig.txt";
+        String filePath2 = "D:\\JAVA\\demo\\PaperChech\\src\\main\\resources\\txt文件\\测试文本\\orig_0.8_add.txt";
+        String txtString1 = TxtUtils.getTxtString(filePath1);
+        String txtString2 = TxtUtils.getTxtString(filePath2);
+        String simHash1 = SimHashUtils.getSimHash(txtString1);
+        String simHash2 = SimHashUtils.getSimHash(txtString2);
+        System.out.println(simHash1);
+        System.out.println(simHash2);
     }
 }
