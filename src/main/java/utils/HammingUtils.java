5b5b9676a94e89a907ea26cc588ec87fec375362
rujiefenfang <1473551776@qq.com>
2021-09-18 19:38:05 +0800

    第二次提交：计算海明距离

--- src/main/java/utils/HammingUtils.java
+++ src/main/java/utils/HammingUtils.java
@@ -1,4 +1,27 @@
 package utils;
 
 public class HammingUtils {
+
+    /**
+     * 传出两个字符串，计算他们的海明距离
+     *
+     * @param s1
+     * @param s2
+     * @return 海明距离
+     */
+    public static int getHammingDistance(String s1, String s2) {
+        int distance = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                distance++;
+            }
+        }
+        return distance;
+    }
+
+    public static double getSimilarity(String s1, String s2) {
+        int distance = getHammingDistance(s1, s2);
+        double similarity = 0.01 * (100 - (double)distance * 100 / 128);
+        return similarity;
+    }
 }
---
+++ src/test/java/HammingUtilsTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import utils.HammingUtils;
+import utils.SimHashUtils;
+import utils.TxtUtils;
+
+public class HammingUtilsTest {
+
+    @Test
+    public void getSimilarityTest(){
+        String filePath1 = "D:\\JAVA\\demo\\PaperChech\\src\\main\\resources\\txt文件\\测试文本\\orig.txt";
+        String filePath2 = "D:\\JAVA\\demo\\PaperChech\\src\\main\\resources\\txt文件\\测试文本\\orig_0.8_add.txt";
+        String txtString1 = TxtUtils.getTxtString(filePath1);
+        String txtString2 = TxtUtils.getTxtString(filePath2);
+        String simHash1 = SimHashUtils.getSimHash(txtString1);
+        String simHash2 = SimHashUtils.getSimHash(txtString2);
+        System.out.println(HammingUtils.getSimilarity(simHash1,simHash2));
+    }
+}
